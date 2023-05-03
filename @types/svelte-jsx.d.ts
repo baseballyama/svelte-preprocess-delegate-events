4083135faaf3bfe6250e236e037442758a147dd8
baseballyama <xydybaseball@gmail.com>
2023-05-03 18:02:54 +0900

    add types

---
+++ @types/svelte-jsx.d.ts
@@ -0,0 +1,10 @@
+declare namespace svelteHTML {
+  /**
+   * base: https://github.com/sveltejs/language-tools/blob/651db67858d18ace44d000d263ac57ed5590ea05/packages/svelte2tsx/svelte-jsx.d.ts#L42
+   */
+  type HTMLProps<Property extends string, Override> =
+    Omit<
+      Omit<import('svelte/elements').SvelteHTMLElements[Property], keyof EventsWithColon<Omit<svelte.JSX.IntrinsicElements[Property & string], svelte.JSX.AttributeNames>>> & EventsWithColon<Omit<svelte.JSX.IntrinsicElements[Property & string], svelte.JSX.AttributeNames>>,
+      keyof Override
+    > & Override & (Record<'on:*' | `on:${string},${string}`, (event: Event & { currentTarget: EventTarget & EventTarget}) => any | never> | object);
+}
