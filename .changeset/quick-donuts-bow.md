9bbcc700f57bd6e980720af52e8faeeea18cfaa0
Yuichiro Yamashita <xydybaseball@gmail.com>
2023-05-07 13:07:19 +0900

    fix: type exports (#17)
    * fix type exports
    
    * add changeset

---
+++ .changeset/quick-donuts-bow.md
@@ -0,0 +1,5 @@
+---
+'svelte-preprocess-delegate-events': patch
+---
+
+fix type exports
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import * as preprocess from './preprocess/index.js';
+import preprocess from './preprocess/index.js';
 import * as runtime from './runtime/index.js';
 
 export { preprocess, runtime };
