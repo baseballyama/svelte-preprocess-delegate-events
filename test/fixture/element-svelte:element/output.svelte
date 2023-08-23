6a15ec8c11d6715d061d2d30b3ce6a277d77c40a
Yuichiro Yamashita <xydybaseball@gmail.com>
2023-08-23 09:42:45 +0900

    fix: fix to generate variable name (#53)

---
+++ test/fixture/element-svelte:element/output.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { boundElements } from 'svelte-preprocess-delegate-events/runtime';
+  import { registerDelegatedEvents } from 'svelte-preprocess-delegate-events/runtime';
+  import { get_current_component } from 'svelte/internal';
+  export let tag = 'p';
+
+  const svelte_element0 = boundElements();
+  const component0 = get_current_component();
+  $: registerDelegatedEvents(svelte_element0.bounds, component0, (handler) => handler, {});
+</script>
+
+<svelte:element this={tag} bind:this={svelte_element0.bounds}>Click Me</svelte:element>
