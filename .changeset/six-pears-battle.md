452be3718b5ddf53b048c67c97bc4b5237c8493b
Yuichiro Yamashita <xydybaseball@gmail.com>
2024-05-09 21:14:38 +0900

    fix: do not duplicated variable names when multiple `svelte:component` exist (#121)

---
+++ .changeset/six-pears-battle.md
@@ -0,0 +1,5 @@
+---
+'svelte-preprocess-delegate-events': patch
+---
+
+fix: do not duplicated variable names when multiple svelte:component exist
---
+++ test/fixture/svelte:component/input.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import Component from './Component.svelte';
+  import Component2 from './Component2.svelte';
+
+  const a = false;
+</script>
+
+{#if a}
+  <svelte:component this={Component} on:* />
+{:else}
+  <svelte:component this={Component2} on:* />
+{/if}
